564323754cb7822bc2e5dcf465be956b3ad1f1a6
rk <471091127@qq.com>
2018-01-01 22:09:33 +0800

    components

---
+++ src/components/detail/detail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './detail.css'
+const detail = React.createClass({
+    render:function(){
+      return <p class='detail'>hello,detail</p>
+    }
+})
+export default detail
