e2416524587399eba90ca66139bda3bc0cc87245
tobeapro <471091127@qq.com>
2020-04-14 17:43:31 +0800

    commit:babel and upgrade

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,11 @@ module.exports = {
         '@babel/preset-typescript'
     ],
     plugins:[
-        '@babel/plugin-transform-runtime'
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                "corejs": 3
+            }
+        ]
     ]
 }
